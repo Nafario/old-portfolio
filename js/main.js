c66e6ed8b44de627e32709f45a8387edb29e6250
Nafario <59171020+Nafario@users.noreply.github.com>
2020-05-17 23:22:22 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ menu.addEventListener("click", () => {
     nav.classList.remove("nav-list");
   }
 });
-
+let logo = document.querySelector('.my-logo')
 let root = document.documentElement;
 const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
@@ -17,9 +17,11 @@ light.addEventListener("click", () => {
   root.style.setProperty("--light", "#ECE9E5");
   root.style.setProperty("--dark", "#403E4B");
   light.style.border = "1px solid #403E4B";
+  logo.src = 'imgs/Logo2.png'
 });
 dark.addEventListener("click", () => {
   root.style.setProperty("--light", "#403E4B");
   root.style.setProperty("--dark", "#ECE9E5");
   dark.style.border = "1px solid #ECE9E5";
+  logo.src = 'imgs/Logo3.png'
 });
